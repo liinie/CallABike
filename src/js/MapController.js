c414055bd8b890ba6a202e418e6b529297c7d99c
Daniel Bischoff <danielbischoff89@gmail.com>
2014-11-19 15:53:43 +0200

    Fixed jumping map after entering search entry and implemented transform checking for both zoom event and transition.

--- src/js/MapController.js
+++ src/js/MapController.js
@@ -41,6 +41,18 @@ MapController = function (mapContainerDiv) {
     
     var innerMapHeigth = this.map.svg.attr("height");
     
+    var checkTransform = function (translate, scale) {
+        var t = translate;
+        var s = scale;
+        var width  = document.getElementById("map-container").offsetWidth;
+        var height = document.getElementById("map-container").offsetHeight;
+        var h = 50;
+        
+        t[0] = Math.min(0, Math.max(width  * (1 - s), t[0]));
+        t[1] = Math.min(height/2 * (s - 1) + h * s, Math.max(height * (1 - s) - h * s, t[1]));
+        return {translate: t, scale: s};
+    }
+    
     this.renderProduct = function ( product ) {
         var arcsnbubbles = dataPreparator.getRenderDataFromProduct(product);
         this.map.arc(arcsnbubbles.arcs,
@@ -75,54 +87,26 @@ MapController = function (mapContainerDiv) {
         t[0] = -(s*(bordersXY_min[0] + (bordersXY_max[0] - bordersXY_min[0])/2) - w/2);
         t[1] = -(s*(bordersXY_min[1] + (bordersXY_max[1] - bordersXY_min[1])/2) - h/2);
         
+        var checked = checkTransform(t,s);
+        t = checked.translate;
+        s = checked.scale;
         
-        //t[0] = bordersXY_min[0] - (bordersXY_max[0] - bordersXY_min[0])/4;
-        //t[1] = (bordersXY_max[1] - bordersXY_min[1])/2;
+        mapController.zoom.translate(t);
+        mapController.zoom.scale(s);
         
         mapController.map.svg.selectAll('g').transition().duration(2000).attr("transform", "translate(" + t + ")scale(" + s + ")").style("stroke-width", 1 / s);
     }
-
-    // setTransform
-    // sets the transform of the svg object
-    /*this.setTransform = function ( translate, scale ) {
-        var width = this.map.svg.attr("width");
-        var height = this.map.svg.attr("height");
-        // WARNING: translation limits do not work if we zoom in...
-//        if ( translate[0] < (1-scale)*width ) {
-//            translate[0] = (1-scale)*width;
-//        }
-//        if ( translate[0] > 0 ) {
-//            translate[0] = 0;
-//        }
-//        
-//        if ( translate[1] > 0 ) {
-//            translate[1] = 0;
-//        }
-//        else if ( translate[1] < (1-scale)*height + (height-innerMapHeigth) ) {
-//            translate[1] = (1-scale)*height + (height-innerMapHeigth);
-//        }
-        currentTransform = { translate: translate, scale: scale}; // cache current transform for later use
-        // apply this transform to the svg
-        this.map.svg.selectAll('g').attr(
-            "transform", "translate("+ 
-            currentTransform.translate + 
-            ")scale(" + 
-            currentTransform.scale + 
-            ")");
-    };*/
     
     // zoomEvent
     // handles the zoom event
     this.zoomEvent = function () {
         var t = d3.event.translate;
         var s = d3.event.scale;
-        var width  = document.getElementById("map-container").offsetWidth;
-        var height = document.getElementById("map-container").offsetHeight;
-        var h = 50;
-
-        t[0] = Math.min(0, Math.max(width  * (1 - s), t[0]));
-        t[1] = Math.min(height/2 * (s - 1) + h * s, Math.max(height * (1 - s) - h * s, t[1]));
-
+        
+        var checked = checkTransform(t,s);
+        t = checked.translate;
+        s = checked.scale;
+        
         mapController.zoom.translate(t);
         mapController.map.svg.selectAll('g').attr("transform", "translate(" + t + ")scale(" + s + ")").style("stroke-width", 1 / s);
         
@@ -132,4 +116,5 @@ MapController = function (mapContainerDiv) {
         .scaleExtent([1,8])
         .on("zoom", this.zoomEvent);
     this.map.svg.call(this.zoom);
+    
 }
