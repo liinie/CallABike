3c846ecaa0f9ccbf006d1e27f84c79003c26a7e8
Daniel Bischoff <danielbischoff89@gmail.com>
2014-11-25 15:11:41 +0200

    Added the interface for the BubbleDrawer

--- src/js/BubbleDrawer.js
+++ src/js/BubbleDrawer.js
@@ -1,3 +1,7 @@
-BubbleDrawer = function () {
-
-}
+BubbleDrawer = function (map_) {
+    var map = map_;
+    
+    this.drawBubbles = function (bubblesArray, options) {
+        map.bubbles(bubblesArray, options);
+    };
+};
