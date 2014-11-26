4cda4a0b25903c6def75856a9fed816c9230993e
Herrdum Steffen <herrdus1@niksula.hut.fi>
2014-11-26 17:39:26 +0200

    added core-classes templates and documentation

--- src/js/instance-creation.js
+++ src/js/instance-creation.js
@@ -1,3 +1,26 @@
+
+
+prod = new Product();
+prod.name                               = "";
+prod.manufacturer = manufacturers({name:  ""}).first();
+prod.components.push(products({name:      ""}).first());
+prod.carbonFootprint                    = ;
+prod.price.productPrice                 = ;
+prod.price.energyCosts                  = ;
+prod.price.manufacturingCosts           = ;
+prod.price.materialCosts                = ;
+products.insert(prod);
+
+man = new Manufacturer();
+man.name =     "";
+man.setAddress("");
+man.workingConditions.employmentProtection  = ;
+man.workingConditions.salery                = ;
+man.workingConditions.workingHours          = ;
+manufacturers.insert(man);
+
+
+
 var products = TAFFY();
 var manufacturers = TAFFY();
 var retailers = TAFFY();
@@ -50,20 +73,17 @@ manufacturers.insert(man);
 var prod = new Product();
 prod.name = "8 GB DDR3 RAM";
 prod.type = "electronics.computers.components.memory";
-prod.id = "1236";
 prod.manufacturer = manufacturers({name: "Mike's Mouse and Computer Supply"}).first();
 products.insert(prod);
 
 prod = new Product();
 prod.name = "13\" Display Glass";
 prod.type = "";
-prod.id = "";
 prod.manufacturer = manufacturers({name: "Orangutan Display Glasses"}).first();
 products.insert(prod);
 
 prod = new Product();
 prod.name = "13\" LED Display Module";
-prod.id = "";
 prod.type = "";
 prod.manufacturer = manufacturers({name: "NextGen LED Display Technologies"}).first();
 products.insert(prod);
@@ -71,7 +91,6 @@ products.insert(prod);
 prod = new Product();
 prod.name = "13\" Display";
 prod.type = "electronics.computers.components.displays";
-prod.id = "1230";
 prod.manufacturer = manufacturers({name: "Crystal Clear Displays"}).first();
 prod.components.push(products({name: "13\" Display Glass"}).first());
 prod.components.push(products({name: "13\" LED Display Module"}).first());
@@ -80,14 +99,12 @@ products.insert(prod);
 prod = new Product();
 prod.name = "15\" Display";
 prod.type = "electronics.computers.components.displays";
-prod.id = "1233";
 prod.manufacturer = manufacturers({name: "Crystal Clear Displays"}).first();
 products.insert(prod);
 
 prod = new Product();
 prod.name = "2.8 GHz e7 Processor";
 prod.type = "electronics.computers.components.processors";
-prod.id = "1231";
 prod.manufacturer = manufacturers({name: "HighSpeed Processors"}).first();
 products.insert(prod);
 
@@ -103,7 +120,6 @@ products.insert(prod);
 prod = new Product();
 prod.name = "MapBook 15\"";
 prod.type = "electronics.computers.laptop";
-prod.id = "1235";
 prod.manufacturer = manufacturers({name: "Badgerconn Electronics"}).first();
 prod.components.push(products({name: "15\" Display"}).first());
 prod.components.push(products({name: "8 GB DDR3 RAM"}).first());
