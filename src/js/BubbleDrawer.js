190ff11e8e81ba2cb5473789769c040fbd946a80
Daniel Bischoff <danielbischoff89@gmail.com>
2014-11-26 17:55:41 +0200

    Added mousover and mouseout functions.

--- src/js/BubbleDrawer.js
+++ src/js/BubbleDrawer.js
@@ -1,20 +1,38 @@
 BubbleDrawer = function (map_) {
     var map = map_;
+    var bubbleConfig = {
+        radiusSmall: 6,
+        radiusLarge: 20,
+        enlargingDuration: 700
+    };
     
     // create a g node to append all bubbles to
     map.svg.append('g').attr('class','bubbles');
     
     this.drawBubbles = function (bubblesArray, options) {
+        var mouseoverfun = function() {
+            d3.select(this)
+            .transition()
+            .duration(bubbleConfig.enlargingDuration)
+            .attr('r',bubbleConfig.radiusLarge);
+        };
+        var mouseoutfun = function () {
+            d3.select(this)
+            .transition(bubbleConfig.enlargingDuration)
+            .attr('r', bubbleConfig.radiusSmall);
+        };
         for (var idx in bubblesArray) {
             var latLng = map.latLngToXY(bubblesArray[idx].latitude, bubblesArray[idx].longitude);
             map.svg.select('g.bubbles').append('svg:circle')
+            .on("mouseover", mouseoverfun)
+            .on("mouseout", mouseoutfun)
             .attr('class','map-bubble')
             .attr('cx', latLng[0])
             .attr('cy', latLng[1])
             .attr('r', 0)
             .transition()
-            .duration(500)
-            .attr('r',6);
+            .duration(bubbleConfig.enlargingDuration)
+            .attr('r',bubbleConfig.radiusSmall);
         }
     };
 };
